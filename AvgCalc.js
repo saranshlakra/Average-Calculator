a7d6f61f584200087b7d636d17ffefedf2e72222
saranshlakra <saranshlakra4@gmail.com>
2020-01-30 14:56:07 +0530

    Update AvgCalc.js
    Now you will see the numbers you entered with the result.

--- AvgCalc.js
+++ AvgCalc.js
@@ -2,17 +2,4 @@ var a, b, c;
 a = parseFloat(prompt("add first value"));
 b = parseFloat(prompt("add second value"));
 c = (a + b) / 2;
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log(c);
+console.log('Average of ' + a + ' ' + 'and' + ' ' + b + ' ' + 'is: ' + c);
